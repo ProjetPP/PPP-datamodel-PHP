e6898167891848ac7f43e2ca2a84b6d32e447603
Tpt <thomaspt@hotmail.fr>
2014-11-09 17:07:58 +0100

    Allows to inject (de)serializers for custom nodes

--- src/SerializerFactory.php
+++ src/SerializerFactory.php
@@ -22,20 +22,27 @@ class SerializerFactory {
 	 */
 	private $nodeSerializer = null;
 
-	public function __construct() {
-		$this->nodeSerializer = $this->buildNodeSerializer();
+	/**
+	 * @param Serializer[] $customNodesSerializers
+	 */
+	public function __construct(array $customNodesSerializers = array()) {
+		$this->nodeSerializer = $this->buildNodeSerializer($customNodesSerializers);
 	}
 
-	private function buildNodeSerializer() {
-		return new DispatchingSerializer(array(
-			new MissingNodeSerializer(),
-			new TripleNodeSerializer($this),
-			new SentenceNodeSerializer(),
-			new BasicResourceNodeSerializer('boolean'),
-			new StringResourceNodeSerializer(),
-			new TimeResourceNodeSerializer()
-
-		));
+	private function buildNodeSerializer(array $customNodesSerializers) {
+		return new DispatchingSerializer(
+			array_merge(
+				$customNodesSerializers,
+				array(
+					new MissingNodeSerializer(),
+					new TripleNodeSerializer($this),
+					new SentenceNodeSerializer(),
+					new BasicResourceNodeSerializer('boolean'),
+					new StringResourceNodeSerializer(),
+					new TimeResourceNodeSerializer()
+				)
+			)
+		);
 	}
 
 	/**
